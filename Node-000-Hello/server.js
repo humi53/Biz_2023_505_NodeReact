824871fc248e0be25ae05111f285846d72e7b723
humi53 <j.k.humi9@gmail.com>
2023-08-02 11:49:04 +0900

    first react

---
+++ Node-000-Hello/server.js
@@ -0,0 +1,9 @@
+import http from "http";
+// const http = require("http")
+
+http
+  .createServer((req, res) => {
+    console.log("Hello My Server");
+    res.end("<h1>hello</h1>");
+  })
+  .listen(8080);
