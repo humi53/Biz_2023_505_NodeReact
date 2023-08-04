6b4e01d8cda04f707622da66a3f7cbca34318f4c
humi53 <j.k.humi9@gmail.com>
2023-08-04 09:21:03 +0900

    todo-03

---
+++ react-001-todolist-03/src/data/SampleTodo.js
@@ -0,0 +1,39 @@
+const SampleData = [
+  {
+    id: 0,
+    sdate: "2023-08-03",
+    stime: "10:00:00",
+    content: "React 학습하기",
+    complete: true,
+  },
+  {
+    id: 1,
+    sdate: "2023-08-03",
+    stime: "10:20:00",
+    content: "TodoList",
+    complete: true,
+  },
+  {
+    id: 2,
+    sdate: "2023-08-03",
+    stime: "10:30:00",
+    content: "vanilla Js 학습",
+    complete: false,
+  },
+  {
+    id: 3,
+    sdate: "2023-08-03",
+    stime: "10:40:00",
+    content: "구조체 학습",
+    complete: false,
+  },
+  {
+    id: 4,
+    sdate: "2023-08-03",
+    stime: "10:50:00",
+    content: "알고리즘 학습하기",
+    complete: false,
+  },
+];
+
+export { SampleData };
