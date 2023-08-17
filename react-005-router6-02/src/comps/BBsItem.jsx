927988ba68819e55f8223977593c167c9860bfd7
humi53 <j.k.humi9@gmail.com>
2023-08-17 13:22:49 +0900

    Props_Drilling

---
+++ react-005-router6-02/src/comps/BBsItem.jsx
@@ -0,0 +1,12 @@
+const BBsItem = ({ item }) => {
+  return (
+    <tr>
+      <td>{item.id}</td>
+      <td>{item.bDate}</td>
+      <td>{item.bSubject}</td>
+      <td>{item.bWriter}</td>
+      <td>0</td>
+    </tr>
+  );
+};
+export default BBsItem;
